805463e90afdf1739b1596c2b4de4a165900e506
refabr1k <48404996+refabr1k@users.noreply.github.com>
2019-08-20 14:27:50 +0800

    Update samba_checker.sh

--- Tools/samba_checker.sh
+++ Tools/samba_checker.sh
@@ -11,7 +11,7 @@
 #Notes:
 # Will sometimes not capture or will print multiple
 # lines. May need to run a second time for success.
-if [ -z $1 ]; then echo "Usage: ./smbver.sh RHOST {RPORT}" && exit; else rhost=$1; fi 
+if [ -z $1 ]; then echo "Usage: $0 <ipaddress> <port>" && exit; else rhost=$1; fi 
 if [ ! -z $2 ]; then rport=$2; else rport=139; fi
 tcpdump -s0 -n -i tap0 src $rhost and port $rport -A -c 7 2>/dev/null | grep -i "samba\|s.a.m" | tr -d '.' | grep -oP 'UnixSamba.*[0-9a-z]' | tr -d '\n' & echo -n "$rhost: " & 
 echo "exit" | smbclient -L $rhost 1>/dev/null 2>/dev/null
